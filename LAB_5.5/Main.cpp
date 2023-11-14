033886379f5fc14885b61981f70664e16bdf1a3c
Demian Rymar <ua.demyan@gmail.com>
2023-11-14 12:09:54 +0200

    Fixed

--- LAB_5.5/Main.cpp
+++ LAB_5.5/Main.cpp
@@ -1,36 +1,77 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int C(int n, int k)
+int C(int n, int k, int level, int& depth)
 {
-	if (k == n or k == 0)
-		return 1;
-	else
-		return C(n - 1, k - 1) + C(n - 1, k);
+    if (level > depth)
+        depth = level;
+
+    // вивід значення поточного рівня рекурсії
+    cout << " level = " << level << endl;
+
+    if (k == n or k == 0)
+        return 1;
+    else
+        return C(n - 1, k - 1, level + 1, depth) + C(n - 1, k, level + 1, depth);
 }
-double b(const int n)
+
+double b(const int n, int level, int& depth)
 {
-	double p = 0;
-	for (int k = 0; k <= n * 1. / 2; k++)
-		p += C(n - k, k);
-	return p;
+    if (level > depth)
+        depth = level;
+
+    // вивід значення поточного рівня рекурсії
+    cout << " level = " << level << endl;
+
+    double p = 0;
+    for (int k = 0; k <= n * 1. / 2; k++)
+        p += C(n - k, k, level + 1, depth);
+    return p;
 }
-int f(int n)
+
+int f(int n, int level, int& depth)
 {
-	if (n == 0 or n == 1)
-		return 1;
-	else
-		return f(n - 1) + f(n - 2);
+    if (level > depth)
+        depth = level;
+
+    // вивід значення поточного рівня рекурсії
+    cout << " level = " << level << endl;
+
+    if (n == 0 or n == 1)
+        return 1;
+    else
+        return f(n - 1, level + 1, depth) + f(n - 2, level + 1, depth);
 }
+
 int main()
 {
-	int n;
-	int k;
-	cout << "n = "; cin >> n;
-	cout << "k = "; cin >> k;
-	cout << endl;
-	cout << "fn = " << f(n) << endl;
-	cout << "b = " << b(n) << endl;
+    int n;
+    int k;
+    cout << "n = ";
+    cin >> n;
+    cout << "k = ";
+    cin >> k;
+    cout << endl;
+
+    int depth = 0; // початкове значення глибини
+    int level = 1; // початкове значення рівня рекурсії
+
+    cout << "C(n, k) = " << C(n, k, level, depth) << endl;
+    cout << "depth = " << depth << endl;
+
+    depth = 0; // обнуляємо глибину для інших функцій
+    level = 1; // обнуляємо рівень рекурсії для інших функцій
+
+    cout << "b(n) = " << b(n, level, depth) << endl;
+    cout << "depth = " << depth << endl;
+
+    depth = 0; // обнуляємо глибину для інших функцій
+    level = 1; // обнуляємо рівень рекурсії для інших функцій
+
+    cout << "f(n) = " << f(n, level, depth) << endl;
+    cout << "depth = " << depth << endl;
+
+    return 0;
 }
